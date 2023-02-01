207c9395e246102c886dd6031a1efe577bd82e2e
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-02-01 19:44:42 +0530

    Updated pom.xml by adding mysql-connector-java,removed mongodb and properties in application.properties also updated ! now using datasource for mysql db

--- src/main/java/com/example/movies_backend/SpringBootApplication.java
+++ src/main/java/com/example/movies_backend/SpringBootApplication.java
@@ -1,9 +1,8 @@
 package com.example.movies_backend;
-
 import org.springframework.boot.SpringApplication;
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
-public class SpringBootApplication {
+public class SpringBootApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApplication.class, args);
 	}
---
+++ src/main/java/com/example/movies_backend/controller/EntryController.java
@@ -0,0 +1,7 @@
+package com.example.movies_backend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EntryController{
+}
