45f417920c6d57f21b7d0c04849f31713e0db614
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-02-01 17:19:48 +0530

    Initial setup done and minimal dependencies are added

---
+++ src/test/java/com/example/movies_backend/SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movies_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
